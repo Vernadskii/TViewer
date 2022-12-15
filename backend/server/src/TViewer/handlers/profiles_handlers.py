f7620f0cbd34ec456a2b15cebaea54f20ac1db51
Vernadskii <d.vernadsky@yandex.ru>
2022-12-15 18:09:36 +0200

    1 create backend template with swagger (#2)
    * Added yaml file for swagger codegen
    
    * Corrected historical-data endpoint
    
    * Picked a new API generator
    
    * Added handlers and corrected api yaml file

---
+++ backend/server/src/TViewer/handlers/profiles_handlers.py
@@ -0,0 +1,11 @@
+def handle_get_profiles():
+    pass
+
+
+def handle_profiles_name_configuration_get(profile_name: str):
+    pass
+
+
+def handle_profiles_name_historical_data_get(
+        profile_name: str, indicator: str, timeframe: int):
+    pass
